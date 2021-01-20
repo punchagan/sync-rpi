ff5d011b80e0dfdad5bcd3f0c75350738ad26dd1
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-01-20 16:20:54 +0530

    phone: Add checks to ensure deps installed & adb connect works

--- phone.sh
+++ phone.sh
@@ -6,14 +6,21 @@ set -e
 
 # FIXME: Hard-coded phone IP, should be able to discover it?
 PHONE_IP=192.168.1.100
+ADB_PORT=5555
 TARGET_DIR=/mnt/hdd/PhoneBackup
 SRC_DIRS=("DCIM" "Download" "Notes" "Pictures" "Record" "VoiceRecorder" "WhatsApp")
 
+# Check if adb exists
+command -v adb || (echo "Please Install adb: sudo apt install adb" && false)
+
 # Check adb-sync exists
-command -v adb-sync
+command -v adb-sync || (echo "Please Install adb-sync: https://github.com/google/adb-sync and ensure it is on \$PATH" && false)
+
+# Check if Phone is connected
+ping -q -c 1 -W 3 $PHONE_IP || (echo "Incorrect Phone IP address: $PHONE_IP" && false)
 
 # Connect to phone
-adb connect "$PHONE_IP:5555"
+adb connect "$PHONE_IP:$ADB_PORT" | sed '/connected to/!{q42}'
 
 # Sync videos
 for dir in ${SRC_DIRS[@]}; do
