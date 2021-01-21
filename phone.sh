b3d80076597619c055276494e53ce2007c6c5b6c
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-01-21 11:02:16 +0530

    phone: Extract system setup checks to a function

--- phone.sh
+++ phone.sh
@@ -10,14 +10,20 @@ ADB_PORT=5555
 TARGET_DIR=/mnt/hdd/PhoneBackup
 SRC_DIRS=("DCIM" "Download" "Notes" "Pictures" "Record" "VoiceRecorder" "WhatsApp")
 
-# Check if adb exists
-command -v adb || (echo "Please Install adb: sudo apt install adb" && false)
 
-# Check adb-sync exists
-command -v adb-sync || (echo "Please Install adb-sync: https://github.com/google/adb-sync and ensure it is on \$PATH" && false)
+function check_setup () {
+    # Check if adb exists
+    command -v adb || (echo "Please Install adb: sudo apt install adb" && false)
 
-# Check if Phone is connected
-ping -q -c 1 -W 3 $PHONE_IP || (echo "Incorrect Phone IP address: $PHONE_IP" && false)
+    # Check adb-sync exists
+    command -v adb-sync || (echo "Please Install adb-sync: https://github.com/google/adb-sync and ensure it is on \$PATH" && false)
+
+    # Check if Phone is connected
+    ping -q -c 1 -W 3 $PHONE_IP || (echo "Incorrect Phone IP address: $PHONE_IP" && false)
+}
+
+# Check system setup before
+check_setup
 
 # Connect to phone
 adb connect "$PHONE_IP:$ADB_PORT" | sed '/connected to/!{q42}'
