8c9b0a7d349431dd3bd314110f5fbf6c94ab5dcc
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-08-10 08:45:11 +0530

    phone: Whatsapp data directory keeps changing?!

--- phone.sh
+++ phone.sh
@@ -9,7 +9,7 @@ source .envrc
 
 ADB_PORT=5555
 TARGET_DIR=/mnt/backup-drive/PhoneBackup
-SRC_DIRS=("DCIM" "Download" "Notes" "Pictures" "Recorders" "Android/media/com.whatsapp/WhatsApp")
+SRC_DIRS=("DCIM" "Download" "Notes" "Pictures" "Recorders" "WhatsApp" "Android/media/com.whatsapp")
 
 function check_setup () {
     # Check if adb exists
