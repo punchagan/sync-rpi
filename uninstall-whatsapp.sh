232d456bbfd6e753ebeb5c3053913dd9e1a87be2
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-08-12 10:44:45 +0530

    phone: Don't backup phone data to rpi when uninstalling WA

--- uninstall-whatsapp.sh
+++ uninstall-whatsapp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Script to backup whatsapp data and uninstall it
+# Script to backup whatsapp data (locally, on the phone) and uninstall it
+## The periodic phone backup will take care of copying the data out of the
+## phone, asynchronously.
 
 set -euo pipefail
 
 pushd $(dirname $0)
 ./make-whatsapp-backup.sh
-./phone.sh
 adb shell cmd package uninstall -k com.whatsapp
